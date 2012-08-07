0f83b85de6fc9fdd4241beaa42e166ffccf8e32a
zhangrentongxuel@163.com <ren.zhangr@ALIBABA-39036.hz.ali.com>
2012-08-07 18:08:02 +0800

    by zhangren!

---
+++ common/src/main/java/com/qth/dal/dao/impl/AdminUserDaoImpl.java
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+package com.qth.dal.dao.impl;
+
+import com.qth.dal.dao.AdminUserDao;
+
+/**
+ * @author ren.zhangr
+ *
+ */
+public class AdminUserDaoImpl implements AdminUserDao {
+
+}
---
+++ common/src/main/java/com/qth/dal/dataobject/BaseDo.java
@@ -0,0 +1,56 @@
+/**
+ *
+ */
+package com.qth.dal.dataobject;
+
+import java.util.Date;
+
+/**
+ * @author ren.zhangr
+ *
+ */
+public class BaseDo {
+	private long id;
+	private Date gmtCreated;
+	private Date gmtModified;
+	private String creator;
+	private String modifier;
+	private String isDeleted;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Date getGmtCreated() {
+		return gmtCreated;
+	}
+	public void setGmtCreated(Date gmtCreated) {
+		this.gmtCreated = gmtCreated;
+	}
+	public Date getGmtModified() {
+		return gmtModified;
+	}
+	public void setGmtModified(Date gmtModified) {
+		this.gmtModified = gmtModified;
+	}
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	public String getModifier() {
+		return modifier;
+	}
+	public void setModifier(String modifier) {
+		this.modifier = modifier;
+	}
+	public String getIsDeleted() {
+		return isDeleted;
+	}
+	public void setIsDeleted(String isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+
+}
