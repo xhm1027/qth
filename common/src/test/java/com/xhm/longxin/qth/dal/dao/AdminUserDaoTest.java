6137ffa01125af29101764631caf27790ad0dc50
xhm1027 <xhm1027@gmail.com>
2012-08-12 20:40:29 +0800

    UT testAdminUserUpdate:use jtester

--- common/src/test/java/com/xhm/longxin/qth/dal/dao/AdminUserDaoTest.java
+++ common/src/test/java/com/xhm/longxin/qth/dal/dao/AdminUserDaoTest.java
@@ -36,23 +36,24 @@ public class AdminUserDaoTest extends BaseDaoTest {
 	}
 
 	@Test
+    @DbFit(when = "dbfit/adminUser/prepare_add_adminUser.when.wiki")
 	public void testAdminUserUpdate() {
 		AdminUser user = new AdminUser();
 		String loginId = "admin";
 		user.setLoginId(loginId);
 		// user.setBuyCategory("1,2,3");
-		user.setName("����");
+		user.setName("xhm");
 		user.setPassword("11234");
 		user.setEmail("afb@123.com");
-		Assert.assertTrue(adminUserDao.addAdminUser(user));
+		
+		want.bool(adminUserDao.addAdminUser(user)).isEqualTo(true);
 		AdminUser u = adminUserDao.getAdminUserByLoginId(loginId);
-		// �޸�
-		u.setName("������");
+	
+		u.setName("zr");
 		adminUserDao.updateAdminUser(u);
-		Assert.assertEquals("������",
-				adminUserDao.getAdminUserByLoginId(loginId).getName());
-		// ɾ��
+		want.string(adminUserDao.getAdminUserByLoginId(loginId).getName()).eq("zr");
+		
 		adminUserDao.deleteAdminUserById(u.getId());
-		Assert.assertNull(adminUserDao.getAdminUserByLoginId(loginId));
+		want.object(adminUserDao.getAdminUserByLoginId(loginId)).isNull();
 	}
 }
