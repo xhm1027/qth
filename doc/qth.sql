31b5e3c798b6e8606165c977ac177eb5dadec1a2
ren.zhangr <zhangrentongxuel@163.com>
2012-08-23 17:00:36 +0800

    qth.sql

--- doc/qth.sql
+++ doc/qth.sql
@@ -1,4 +1,4 @@
-Ôªø# MySQL-Front 5.1  (Build 4.2)
+# MySQL-Front 5.1  (Build 4.2)
 
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
 /*!40101 SET SQL_MODE='' */;
@@ -27,7 +27,13 @@ CREATE TABLE `admin_user` (
   `name` varchar(50) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2860 DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table admin_user
+#
+
+INSERT INTO `admin_user` VALUES (2858,'2012-08-15 17:05:18','system','2012-08-15 17:05:18','system','Y','admin','6f04f0d75f6870858bae14ac0b6d9f73','zr','afb@123.com');
 
 #
 # Source for table qth_attachment
@@ -46,7 +52,16 @@ CREATE TABLE `qth_attachment` (
   `owner_id` bigint(20) DEFAULT NULL,
   `key` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2548 DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table qth_attachment
+#
+
+INSERT INTO `qth_attachment` VALUES (2540,NULL,NULL,'2012-08-15 17:05:24',NULL,'Y','image','/test/img1.jpb',1,'sale');
+INSERT INTO `qth_attachment` VALUES (2542,NULL,NULL,'2012-08-15 17:05:24',NULL,'N','image','/test/img2.jpb',1,'sale');
+INSERT INTO `qth_attachment` VALUES (2544,NULL,NULL,'2012-08-15 17:05:24',NULL,'N','image','/test/img3.jpb',1,'sale');
+INSERT INTO `qth_attachment` VALUES (2546,'2012-08-15 17:05:24','system','2012-08-15 17:05:24','system','N','image','/test/img4.jpb',1,'sale');
 
 #
 # Source for table qth_audit_log
@@ -66,7 +81,13 @@ CREATE TABLE `qth_audit_log` (
   `audit_result` varchar(50) DEFAULT NULL,
   `description` varbinary(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=612 DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table qth_audit_log
+#
+
+INSERT INTO `qth_audit_log` VALUES (610,'2012-08-15 17:05:18','system','2012-08-15 17:05:18','system','N','test auditor','user',1,'fail','desc');
 
 #
 # Source for table qth_msg
@@ -88,7 +109,15 @@ CREATE TABLE `qth_msg` (
   `sender_deleted` char(1) DEFAULT NULL,
   `receiver_deleted` char(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=662 DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table qth_msg
+#
+
+INSERT INTO `qth_msg` VALUES (1,NULL,NULL,'2012-08-15 17:05:21',NULL,'N','≤‚ ‘±ÍÃ‚1','s1∏¸–¬','r1','√Ë ˆ–≈œ¢xxx£¨∂Ã–≈ƒ⁄»›','N','N','N');
+INSERT INTO `qth_msg` VALUES (2,NULL,NULL,NULL,NULL,'N','≤‚ ‘±ÍÃ‚2','s2','r2','√Ë ˆ–≈œ¢xxx£¨∂Ã–≈ƒ⁄»›','N','N','N');
+INSERT INTO `qth_msg` VALUES (3,NULL,NULL,NULL,NULL,'N','≤‚ ‘±ÍÃ‚3','s3','r3','√Ë ˆ–≈œ¢xxx£¨∂Ã–≈ƒ⁄»›','N','N','N');
 
 #
 # Source for table qth_pro_category
@@ -106,7 +135,15 @@ CREATE TABLE `qth_pro_category` (
   `description` varchar(500) DEFAULT NULL,
   `is_material` char(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=648 DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table qth_pro_category
+#
+
+INSERT INTO `qth_pro_category` VALUES (1,NULL,NULL,NULL,NULL,'N','≤‚ ‘¿‡±1','√Ë ˆ–≈œ¢xxx','Y');
+INSERT INTO `qth_pro_category` VALUES (2,NULL,NULL,NULL,NULL,'N','≤‚ ‘¿‡±2','√Ë ˆ–≈œ¢xxx','Y');
+INSERT INTO `qth_pro_category` VALUES (3,NULL,NULL,NULL,NULL,'N','≤‚ ‘¿‡±3','√Ë ˆ–≈œ¢xxx','N');
 
 #
 # Source for table qth_product_buy
@@ -131,7 +168,15 @@ CREATE TABLE `qth_product_buy` (
   `product_type` varchar(50) DEFAULT NULL,
   `is_sale` char(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=706 DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table qth_product_buy
+#
+
+INSERT INTO `qth_product_buy` VALUES (1,NULL,NULL,'2012-08-15 17:05:20',NULL,'N','≤‚ ‘≤˙∆∑1',1,NULL,NULL,NULL,NULL,'new','zhangren','resource','Y');
+INSERT INTO `qth_product_buy` VALUES (2,NULL,NULL,NULL,NULL,'N','≤‚ ‘≤˙∆∑2',1,NULL,NULL,NULL,NULL,'new','xhm','resource','Y');
+INSERT INTO `qth_product_buy` VALUES (3,NULL,NULL,NULL,NULL,'N','≤‚ ‘≤˙∆∑3',1,NULL,NULL,NULL,NULL,'new','zhangren','resource','N');
 
 #
 # Source for table qth_product_sale
@@ -156,7 +201,15 @@ CREATE TABLE `qth_product_sale` (
   `product_type` varchar(50) DEFAULT NULL,
   `is_sale` char(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=646 DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table qth_product_sale
+#
+
+INSERT INTO `qth_product_sale` VALUES (1,NULL,NULL,'2012-08-15 17:05:24',NULL,'N','≤‚ ‘≤˙∆∑1',1,NULL,NULL,NULL,NULL,'new','zhangren','resource','Y');
+INSERT INTO `qth_product_sale` VALUES (2,NULL,NULL,NULL,NULL,'N','≤‚ ‘≤˙∆∑2',1,NULL,NULL,NULL,NULL,'new','xhm','resource','Y');
+INSERT INTO `qth_product_sale` VALUES (3,NULL,NULL,NULL,NULL,'N','≤‚ ‘≤˙∆∑3',1,NULL,NULL,NULL,NULL,'new','zhangren','resource','N');
 
 #
 # Source for table qth_user
@@ -186,7 +239,19 @@ CREATE TABLE `qth_user` (
   `modifier` varchar(50) DEFAULT NULL,
   `status` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8018 DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table qth_user
+#
+
+INSERT INTO `qth_user` VALUES (8004,'zhangren',NULL,'–’√˚1','f','abc@abc.com','0571','85022088',NULL,'π´Àæ1',NULL,'innerUser',NULL,NULL,'golden','N',NULL,NULL,'2012-08-16 20:43:16',NULL,'new');
+INSERT INTO `qth_user` VALUES (8006,'xhm',NULL,'–’√˚2','f','abc@abc.com','0571','85022088',NULL,'π´Àæ2',NULL,'innerUser',NULL,NULL,'golden','N',NULL,NULL,NULL,NULL,'new');
+INSERT INTO `qth_user` VALUES (8008,'admin',NULL,'–’√˚3','f','abc@abc.com','0571','85022088',NULL,'π´Àæ3',NULL,'innerUser',NULL,NULL,'golden','N',NULL,NULL,NULL,NULL,'new');
+INSERT INTO `qth_user` VALUES (8010,'zhangren1',NULL,'–’√˚4','f','abc@abc.com','0571','85022088',NULL,'π´Àæ4',NULL,'innerUser',NULL,NULL,'golden','N',NULL,NULL,NULL,NULL,'new');
+INSERT INTO `qth_user` VALUES (8012,'xhm1',NULL,'–’√˚5','f','abc@abc.com','0571','85022088',NULL,'π´Àæ5',NULL,'innerUser',NULL,NULL,'golden','N',NULL,NULL,NULL,NULL,'new');
+INSERT INTO `qth_user` VALUES (8014,'admin1',NULL,'–’√˚6','f','abc@abc.com','0571','85022088',NULL,'π´Àæ6',NULL,'innerUser',NULL,NULL,'golden','N',NULL,NULL,NULL,NULL,'new');
+INSERT INTO `qth_user` VALUES (8016,'addzhangren','6f04f0d75f6870858bae14ac0b6d9f73','µ•≤‚’≈»˝','f','afb@123.com','0571','88889988','12355478965','µ•≤‚π´Àæ√˚','µ•‘™≤‚ ‘π´Àæµÿ÷∑','innerUser','365587458755','11234','common','N','2012-08-16 20:43:16','system','2012-08-16 20:43:16','system','new');
 
 #
 # Source for table qth_user_interest
@@ -197,14 +262,28 @@ CREATE TABLE `qth_user_interest` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `login_id` varchar(50) DEFAULT NULL,
   `interest` varchar(50) DEFAULT NULL,
-  `value` varchar(10) DEFAULT NULL,
+  `value` bigint(20) DEFAULT '0',
   `is_deleted` char(1) DEFAULT NULL,
   `gmt_created` datetime DEFAULT NULL,
   `creator` varchar(50) DEFAULT NULL,
   `gmt_modified` datetime DEFAULT NULL,
   `modifier` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3564 DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table qth_user_interest
+#
+
+INSERT INTO `qth_user_interest` VALUES (3546,'zhangren','buy',1,'Y',NULL,NULL,'2012-08-16 20:43:16',NULL);
+INSERT INTO `qth_user_interest` VALUES (3548,'zhangren','buy',2,'N',NULL,NULL,NULL,NULL);
+INSERT INTO `qth_user_interest` VALUES (3550,'zhangren','sale',3,'N',NULL,NULL,NULL,NULL);
+INSERT INTO `qth_user_interest` VALUES (3552,'xhm1','buy',1,'N',NULL,NULL,NULL,NULL);
+INSERT INTO `qth_user_interest` VALUES (3554,'xhm1','buy',2,'N',NULL,NULL,NULL,NULL);
+INSERT INTO `qth_user_interest` VALUES (3556,'xhm1','sale',3,'N',NULL,NULL,NULL,NULL);
+INSERT INTO `qth_user_interest` VALUES (3558,'xhm','buy',1,'N',NULL,NULL,NULL,NULL);
+INSERT INTO `qth_user_interest` VALUES (3560,'addzhangren','buy',1,'N','2012-08-16 20:43:16','system','2012-08-16 20:43:16','system');
+INSERT INTO `qth_user_interest` VALUES (3562,'zhangren','sale',1,'N','2012-08-16 20:43:16','system','2012-08-16 20:43:16','system');
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
